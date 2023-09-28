605bfe6138328aa67e601b58d96c7d712cc50ec0
alan890104 <alan890104@gmail.com>
2023-09-28 22:49:35 +0800

    - feat(jwt_auth_middleware.go): add AuthMiddleware function to handle JWT authentication - fix(jwt_auth_middleware.go): fix splitting bearer token in AuthMiddleware function

--- api/restful/middleware/jwt_auth_middleware.go
+++ api/restful/middleware/jwt_auth_middleware.go
@@ -1,1 +1,37 @@
 package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/alan890104/go-clean-arch-demo/domain"
+	tokensvc "github.com/alan890104/go-clean-arch-demo/internal/token"
+	"github.com/gin-gonic/gin"
+)
+
+func AuthMiddleware(secret string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		authHeader := c.GetHeader("Authorization")
+		bearerToken := strings.Split(authHeader, "")
+		if len(bearerToken) != 2 {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, domain.Response{
+				Msg: "Invalid token format (length different from 2)",
+			})
+			return
+		}
+		authToken := bearerToken[1]
+		isAuthorized, err := tokensvc.IsAuthorized(authToken, secret)
+		if err != nil {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, domain.Response{
+				Msg: err.Error(),
+			})
+			return
+		}
+		if !isAuthorized {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, domain.Response{
+				Msg: "Invalid token",
+			})
+		}
+		c.Next()
+	}
+}
