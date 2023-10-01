bc7f7ac238bd2b6b5cfc0c95ffd3322ac8b40273
alan890104 <alan890104@gmail.com>
2023-10-02 03:51:29 +0800

    * refactor(gen.go): remove commented out code for custom query interface * feat(gen.go): add basic generation for domain.Book and domain.User

--- cmd/gen/gen.go
+++ cmd/gen/gen.go
@@ -12,12 +12,6 @@ func main() {
 		OutPath: path.Join("domain", "query"),
 		Mode:    gen.WithDefaultQuery | gen.WithQueryInterface,
 	})
-
-	// If you have custom query interface, uncomment these 3 lines
-	// app := bootstrap.App()
-	// gormdb := bootstrap.NewDB(app.Env)
-	// g.UseDB(gormdb)
-
 	g.ApplyBasic(domain.Book{}, domain.User{})
 	g.Execute()
 }
