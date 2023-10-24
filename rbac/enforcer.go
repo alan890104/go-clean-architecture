4d8bab4c2c5aaaa916b788310d8ec69e5ce84ef4
peterxcli <peterxcli@gmail.com>
2023-10-25 02:18:40 +0800

    chore: Simplify file path construction for casbin file

--- rbac/enforcer.go
+++ rbac/enforcer.go
@@ -5,18 +5,11 @@ import (
 	"path/filepath"
 
 	"github.com/casbin/casbin/v2"
-	projectRootDir "github.com/golang-infrastructure/go-project-root-directory"
 )
 
 func NewEnforcer() *casbin.Enforcer {
-	// Get the absolute path to the project directory
-	projectDir, err := projectRootDir.GetRootDirectory()
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-
-	modelPath := filepath.Join(projectDir, "rbac", "model.conf")
-	policyPath := filepath.Join(projectDir, "rbac", "policy.csv")
+	modelPath := filepath.Join("rbac", "model.conf")
+	policyPath := filepath.Join("rbac", "policy.csv")
 
 	enforcer, err := casbin.NewEnforcer(modelPath, policyPath)
 	if err != nil {
